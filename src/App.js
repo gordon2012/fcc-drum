03d8a8e8c15bbdb277420c2675f8168eddbfd57c
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-07-04 08:36:17 -0700

    Add drum elements with clickable sounds
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    playing: null,
+    clips: [
+      { name: 'Q', sound: 'boom' },
+      { name: 'W', sound: 'clap' },
+      { name: 'E', sound: 'hihat' },
+      { name: 'A', sound: 'kick' },
+      { name: 'S', sound: 'openhat' },
+      { name: 'D', sound: 'ride' },
+      { name: 'Z', sound: 'snare' },
+      { name: 'X', sound: 'tink' },
+      { name: 'C', sound: 'tom' }
+    ]
+  };
+
+  playAudio = audio => {
+    this[audio].play();
+    this.setState({ playing: audio });
+  };
+
   render() {
     return (
       <div className="App">
-        <div />
+        <div id="drum-machine">
+          <div id="display">{this.state.playing}</div>
+
+          {this.state.clips.map(drum => {
+            return (
+              <div
+                key={drum.name}
+                className="drum-pad"
+                id={drum.sound}
+                onClick={() => {
+                  this.playAudio(drum.name);
+                }}
+              >
+                {drum.name}
+                <audio
+                  ref={ref => {
+                    this[drum.name] = ref;
+                  }}
+                  src={`audio/${drum.sound}.wav`}
+                  className="clip"
+                  id={drum.name}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
